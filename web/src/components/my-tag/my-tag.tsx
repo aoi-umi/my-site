7f8cce61680ad075b9c435b95a4b3aaddc58dcbb
hb <1641622365@qq.com>
2021-03-30 11:32:14 +0800

    my-tag disabled

--- web/src/components/my-tag/my-tag.tsx
+++ web/src/components/my-tag/my-tag.tsx
@@ -4,21 +4,25 @@ import { Prop } from '@/components/property-decorator'
 
 import { convClass, getCompOpts } from '../utils'
 import { Tag, Tooltip } from '../iview'
+import { MyBase } from '../my-base'
 import { TagType } from './model'
+import './style.less'
 export type RenderTagType = string | TagType;
 
 export class MyTagBaseProp<T = RenderTagType> {
-    @Prop()
-    value?: T | (T[]);
+  @Prop()
+  value?: T | (T[]);
 
-    @Prop()
-    singleCheck?: boolean;
+  @Prop()
+  singleCheck?: boolean;
 }
 @Component({
+  extends: MyBase,
   mixins: [getCompOpts(MyTagBaseProp)]
 })
-export class MyTagBase<T = RenderTagType> extends Vue<MyTagBaseProp<T>> {
-    @Watch('value')
+export class MyTagBase<T = RenderTagType> extends Vue<MyTagBaseProp<T>, MyBase> {
+  stylePrefix = 'my-tag-'
+  @Watch('value')
   protected watchValue (newValue) {
     let newList = []
     if (newValue) { newList = newValue instanceof Array ? newValue : [newValue] }
@@ -41,49 +45,49 @@ export class MyTagBase<T = RenderTagType> extends Vue<MyTagBaseProp<T>> {
     this.tagList = newList
   }
 
-    tagList: T[] = [];
+  tagList: T[] = [];
 
-    created () {
-      this.watchValue(this.value)
-    }
+  created () {
+    this.watchValue(this.value)
+  }
 
-    render () {
-      return this.renderTag(this.tagList)
-    }
+  render () {
+    return this.renderTag(this.tagList)
+  }
 
-    renderTag (tagList: RenderTagType | (RenderTagType[])) {
-      const list = tagList instanceof Array ? tagList : [tagList]
-      return (
-        <div>
-          {list.map(ele => {
-            if (typeof ele === 'string') {
-              return <Tag color='blue'>{ele}</Tag>
-            }
-            if (ele.disabled) { return <Tag color='default'>{ele.tag}</Tag> }
-            return (
-              <Tag color={ele.color as any || 'blue'} checkable={ele.checkable} checked={ele.checked} on-on-change={(checked) => {
-                if (!this.singleCheck) {
-                  ele.checked = checked
-                } else {
-                  list.forEach((ele2: TagType) => {
-                    ele2.checked = false
-                  })
-                  ele.checked = checked
-                }
-                this.$emit('change', checked)
-              }}>
-                {ele.isDel
-                  ? <del>
-                    {ele.tag}
-                  </del>
-                  : ele.tag
-                }
-              </Tag>
-            )
-          })}
-        </div>
-      )
-    }
+  renderTag (tagList: RenderTagType | (RenderTagType[])) {
+    const list = tagList instanceof Array ? tagList : [tagList]
+    return (
+      <div class={this.getStyleName('root')}>
+        {list.map(ele => {
+          if (typeof ele === 'string') {
+            return <Tag color='blue'>{ele}</Tag>
+          }
+          if (ele.disabled) { return <Tag class={this.getStyleName('disabled')} color='default'>{ele.tag}</Tag> }
+          return (
+            <Tag color={ele.color as any || 'blue'} checkable={ele.checkable} checked={ele.checked} on-on-change={(checked) => {
+              if (!this.singleCheck) {
+                ele.checked = checked
+              } else {
+                list.forEach((ele2: TagType) => {
+                  ele2.checked = false
+                })
+                ele.checked = checked
+              }
+              this.$emit('change', checked)
+            }}>
+              {ele.isDel
+                ? <del>
+                  {ele.tag}
+                </del>
+                : ele.tag
+              }
+            </Tag>
+          )
+        })}
+      </div>
+    )
+  }
 }
 
 const MyTagView = convClass<MyTagBaseProp>(MyTagBase)
--- web/src/views/goods/goods-detail.tsx
+++ web/src/views/goods/goods-detail.tsx
@@ -21,224 +21,224 @@ import './goods.less'
 
 @Component
 export default class GoodsDetail extends Base {
-    stylePrefix = 'goods-';
-    private innerDetail: DetailType = {} as any;
-    private async loadDetailData () {
-      const query = this.$route.query
-      const detail = await testApi.goodsDetailQuery({ _id: query._id })
-      this.innerDetail = detail
-      return detail
-    }
+  stylePrefix = 'goods-';
+  private innerDetail: DetailType = {} as any;
+  private async loadDetailData () {
+    const query = this.$route.query
+    const detail = await testApi.goodsDetailQuery({ _id: query._id })
+    this.innerDetail = detail
+    return detail
+  }
 
-    protected render () {
-      return (
-        <div>
-          <MyLoad
-            loadFn={this.loadDetailData}
-            renderFn={(detail: DetailType) => {
-              return <GoodsDetailMainView data={detail} />
-            }}
-            errMsgFn={(e) => {
-              if (getErrorCfgByCode(e.code)) { return '商品不存在或已删除' }
-              return e.message
-            }}
-          />
-        </div>
-      )
-    }
+  protected render () {
+    return (
+      <div>
+        <MyLoad
+          loadFn={this.loadDetailData}
+          renderFn={(detail: DetailType) => {
+            return <GoodsDetailMainView data={detail} />
+          }}
+          errMsgFn={(e) => {
+            if (getErrorCfgByCode(e.code)) { return '商品不存在或已删除' }
+            return e.message
+          }}
+        />
+      </div>
+    )
+  }
 }
 
 class GoodsDetailMainProp {
-    @Prop({
-      required: true
-    })
-    data: DetailType;
+  @Prop({
+    required: true
+  })
+  data: DetailType;
 }
 @Component({
   extends: Base,
   mixins: [getCompOpts(GoodsDetailMainProp)]
 })
 class GoodsDetailMain extends Vue<GoodsDetailMainProp & Base> {
-    stylePrefix = 'goods-';
-    $refs: { imgViewer: IMyImgViewer, pay: Pay };
+  stylePrefix = 'goods-';
+  $refs: { imgViewer: IMyImgViewer, pay: Pay };
 
-    specTag: { name: string, value: TagType[] }[] = [];
-    @Watch('data')
-    protected watchData (newValue: DetailType) {
-      this.specTag = newValue.specGroup.map(e => {
-        return {
-          name: e.name,
-          value: e.value.map((v) => {
-            return {
-              key: v,
-              tag: v,
-              checkable: true
-            }
-          })
-        }
-      })
-    }
-
-    typeList: { key: string; value: any, checked?: boolean }[] = [];
-    created () {
-      this.typeList = myEnum.assetSourceType.toArray().filter(ele => ele.value === myEnum.assetSourceType.支付宝).map(ele => {
-        ele['checked'] = false
-        return ele
-      })
-      this.watchData(this.data)
-      this.selectSpec()
-    }
-
-    sku: SkuType = null;
-    buyInfo = {
-      name: '',
-      payType: myEnum.assetSourceType.支付宝,
-      quantity: 1
-    };
-    get totalPrice () {
-      return mathjs.round((this.sku ? this.sku.price : 0) * this.buyInfo.quantity, 2) as number
-    }
-
-    selectSpec () {
-      const selectSpec = []
-      let selectFinish = true
-      for (let i = 0; i < this.specTag.length; i++) {
-        const ele = this.specTag[i]
-        const match = ele.value.find(v => v.checked)
-        if (match) {
-          selectSpec.push(match.key)
-        } else {
-          selectSpec.push(null)
-          selectFinish = false
-        }
+  specTag: { name: string, value: TagType[] }[] = [];
+  @Watch('data')
+  protected watchData (newValue: DetailType) {
+    this.specTag = newValue.specGroup.map((e, idx) => {
+      return {
+        name: e.name,
+        value: e.value.filter(v => newValue.sku.find(s => s.spec[idx] === v)).map((v) => {
+          return {
+            key: v,
+            tag: v,
+            checkable: true
+          }
+        })
       }
-      if (selectFinish) {
-        this.sku = this.data.sku.find(ele => {
-          let match = true
-          if (ele.spec.length !== this.specTag.length) {
-            match = false
-          } else {
-            for (let idx = 0; idx < selectSpec.length; idx++) {
-              if (selectSpec[idx] !== ele.spec[idx]) {
-                match = false
-                break
-              }
+    }).filter(ele => ele.value.length)
+  }
+
+  typeList: { key: string; value: any, checked?: boolean }[] = [];
+  created () {
+    this.typeList = myEnum.assetSourceType.toArray().filter(ele => ele.value === myEnum.assetSourceType.支付宝).map(ele => {
+      ele['checked'] = false
+      return ele
+    })
+    this.watchData(this.data)
+    this.selectSpec()
+  }
+
+  sku: SkuType = null;
+  buyInfo = {
+    name: '',
+    payType: myEnum.assetSourceType.支付宝,
+    quantity: 1
+  };
+  get totalPrice () {
+    return mathjs.round((this.sku ? this.sku.price : 0) * this.buyInfo.quantity, 2) as number
+  }
+
+  selectSpec () {
+    const selectSpec = []
+    let selectFinish = true
+    for (let i = 0; i < this.specTag.length; i++) {
+      const ele = this.specTag[i]
+      const match = ele.value.find(v => v.checked)
+      if (match) {
+        selectSpec.push(match.key)
+      } else {
+        selectSpec.push(null)
+        selectFinish = false
+      }
+    }
+    if (selectFinish) {
+      this.sku = this.data.sku.find(ele => {
+        let match = true
+        if (ele.spec.length !== this.specTag.length) {
+          match = false
+        } else {
+          for (let idx = 0; idx < selectSpec.length; idx++) {
+            if (selectSpec[idx] !== ele.spec[idx]) {
+              match = false
+              break
             }
           }
-          return match
-        })
-      } else {
-        this.sku = null
-      }
-      this.buyInfo.name = this.sku ? this.data.spu.name + '-' + this.sku.name : ''
-
-      function canBuy (s) {
-        return s.quantity > 0 && s.status === myEnum.goodsSkuStatus.上架
-      }
-      // 不可选项
-      this.specTag.forEach((ele, idx) => {
-        ele.value.forEach(v => {
-          v.disabled = !this.data.sku.find(s => canBuy(s) && s.spec[idx] === v.key)
-        })
-
-        if (selectSpec[idx]) {
-          this.specTag.forEach((ele2, idx2) => {
-            if (idx !== idx2) {
-              ele2.value.forEach((v, vIdx) => {
-                const match = this.data.sku.find(s =>
-                  canBuy(s) &&
-                                s.spec[idx] === selectSpec[idx] && s.spec[idx2] === v.key)
-                if (!match) { v.disabled = true }
-              })
-            }
-          })
         }
+        return match
       })
+    } else {
+      this.sku = null
     }
+    this.buyInfo.name = this.sku ? this.data.spu.name + '-' + this.sku.name : ''
 
-    private async buy () {
-      const rs = await testApi.goodsBuy({
-        quantity: this.buyInfo.quantity,
-        payType: this.buyInfo.payType,
-        totalPrice: this.totalPrice,
-        skuId: this.sku._id
+    function canBuy (s) {
+      return s.quantity > 0 && s.status === myEnum.goodsSkuStatus.上架
+    }
+    // 不可选项
+    this.specTag.forEach((ele, idx) => {
+      ele.value.forEach(v => {
+        v.disabled = !this.data.sku.find(s => canBuy(s) && s.spec[idx] === v.key)
       })
-      return rs
-    }
 
-    showIdx = 0;
-    render () {
-      const { spu } = this.data
-      const multi = spu.imgUrls.length > 1
-      return (
-        <div>
-          <PayView ref='pay' payFn={async () => {
-            return this.buy()
-          }}>
-            <p>{this.buyInfo.name}</p>
-            <p>支付金额: {this.totalPrice.toFixed(2)}</p>
-          </PayView>
-          <h2>{spu.name}</h2>
-          <MyImgViewer ref='imgViewer' src={spu.imgUrls} idx={this.showIdx} />
-          <Row gutter={20}>
-            <Col xs={24} sm={8}>
-              <Carousel class={this.getStyleName('carousel')} loop
-                arrow={multi ? 'hover' : 'never'}
-                dots={multi ? 'inside' : 'none'}
-              >
-                {spu.imgUrls.map((ele, idx) => {
-                  return (
-                    <CarouselItem class={this.getStyleName('carousel-item')}>
-                      <div class={this.getStyleName('carousel-img')} on-click={() => {
-                        this.showIdx = idx
-                        this.$refs.imgViewer.show()
-                      }} >
-                        <MyImg src={ele} />
-                      </div>
-                    </CarouselItem>
-                  )
-                })}
-              </Carousel>
-            </Col>
-            <Col xs={24} sm={16}>
-              {this.specTag.map((ele, idx) => {
+      if (selectSpec[idx]) {
+        this.specTag.forEach((ele2, idx2) => {
+          if (idx !== idx2) {
+            ele2.value.forEach((v, vIdx) => {
+              const match = this.data.sku.find(s =>
+                canBuy(s) &&
+                s.spec[idx] === selectSpec[idx] && s.spec[idx2] === v.key)
+              if (!match) { v.disabled = true }
+            })
+          }
+        })
+      }
+    })
+  }
+
+  private async buy () {
+    const rs = await testApi.goodsBuy({
+      quantity: this.buyInfo.quantity,
+      payType: this.buyInfo.payType,
+      totalPrice: this.totalPrice,
+      skuId: this.sku._id
+    })
+    return rs
+  }
+
+  showIdx = 0;
+  render () {
+    const { spu } = this.data
+    const multi = spu.imgUrls.length > 1
+    return (
+      <div>
+        <PayView ref='pay' payFn={async () => {
+          return this.buy()
+        }}>
+          <p>{this.buyInfo.name}</p>
+          <p>支付金额: {this.totalPrice.toFixed(2)}</p>
+        </PayView>
+        <h2>{spu.name}</h2>
+        <MyImgViewer ref='imgViewer' src={spu.imgUrls} idx={this.showIdx} />
+        <Row gutter={20}>
+          <Col xs={24} sm={8}>
+            <Carousel class={this.getStyleName('carousel')} loop
+              arrow={multi ? 'hover' : 'never'}
+              dots={multi ? 'inside' : 'none'}
+            >
+              {spu.imgUrls.map((ele, idx) => {
                 return (
-                  <div>
-                    <div>
-                      <span class={this.getStyleName('spec-name')}>{ele.name}</span>
+                  <CarouselItem class={this.getStyleName('carousel-item')}>
+                    <div class={this.getStyleName('carousel-img')} on-click={() => {
+                      this.showIdx = idx
+                      this.$refs.imgViewer.show()
+                    }} >
+                      <MyImg src={ele} />
                     </div>
-                    <div>
-                      <MyTag value={ele.value} singleCheck on-change={() => {
-                        this.selectSpec()
-                      }} />
-                    </div>
-                    <Divider />
-                  </div>
+                  </CarouselItem>
                 )
               })}
-            </Col>
-            <Card class={this.getStyleName('buy-box')}>
-              <div class={this.getStyleName('buy-box-cont')}>
-                {this.sku &&
-                                <div class={this.getStyleName('count-box')}>
-                                  <div>
-                                        单价 {this.sku.price} / 库存 {this.sku.quantity}
-                                  </div>
-                                  <MyNumber v-model={this.buyInfo.quantity} min={1} max={this.sku.quantity} />
-                                </div>
-                }
-                <div class={this.getStyleName('buy')}>
-                  <span>总价:{this.totalPrice.toFixed(2)}</span>
-                  <Button disabled={!this.sku} on-click={() => {
-                    this.$refs.pay.toggle(true)
-                  }}>立即购买</Button>
+            </Carousel>
+          </Col>
+          <Col xs={24} sm={16}>
+            {this.specTag.map((ele, idx) => {
+              return (
+                <div>
+                  <div>
+                    <span class={this.getStyleName('spec-name')}>{ele.name}</span>
+                  </div>
+                  <div>
+                    <MyTag value={ele.value} singleCheck on-change={() => {
+                      this.selectSpec()
+                    }} />
+                  </div>
+                  <Divider />
                 </div>
+              )
+            })}
+          </Col>
+          <Card class={this.getStyleName('buy-box')}>
+            <div class={this.getStyleName('buy-box-cont')}>
+              {this.sku &&
+                <div class={this.getStyleName('count-box')}>
+                  <div>
+                    单价 {this.sku.price} / 库存 {this.sku.quantity}
+                  </div>
+                  <MyNumber v-model={this.buyInfo.quantity} min={1} max={this.sku.quantity} />
+                </div>
+              }
+              <div class={this.getStyleName('buy')}>
+                <span>总价:{this.totalPrice.toFixed(2)}</span>
+                <Button disabled={!this.sku} on-click={() => {
+                  this.$refs.pay.toggle(true)
+                }}>立即购买</Button>
               </div>
-            </Card>
-          </Row>
-        </div>
-      )
-    }
+            </div>
+          </Card>
+        </Row>
+      </div>
+    )
+  }
 }
 
 export const GoodsDetailMainView = convClass<GoodsDetailMainProp>(GoodsDetailMain)
