c1b36c515351dfed41313a15c086644a63bdbd2c
my-pc <1641622365@qq.com>
2021-10-28 21:33:52 +0800

    修改环境变量写法

--- web/src/config/env.ts
+++ web/src/config/env.ts
@@ -1,30 +1,11 @@
-
-const host = 'sellfishboy.top'
-const ip = '144.202.99.178'
-// const env = 'prod'
-const env = [host, ip].find(ele => location.hostname.includes(ele)) ? 'prod' : 'dev'
-const envConfig: {
-  [env: string]: {
-    title: string;
-    host: string;
-    apiHost: string;
-  }
-} = {
-  prod: {
-    title: '正式',
-    host: 'http://' + ip,
-    apiHost: `//api.${host}`
-  },
-  dev: {
-    title: '开发',
-    host: `http://127.0.0.1:8080`,
-    apiHost: `//${location.hostname}:8000`
-  }
+export const currEnvCfg = {
+  title: process.env.VUE_APP_TITLE,
+  // host: `http://127.0.0.1:8080`,
+  apiHost: process.env.VUE_APP_API
 }
-export const currEnvCfg = envConfig[env]
 const config = {
   title: currEnvCfg.title,
-  ipHost: currEnvCfg.host,
+  // ipHost: currEnvCfg.host,
   socket: {
     test: {
       host: `${currEnvCfg.apiHost}`,
